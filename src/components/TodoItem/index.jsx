e18fbc6d7d06a5b315e95f38fb101e1e3be33f09
yejine2 <yejine2@naver.com>
2022-08-04 21:17:47 +0900

    feat: Add todo Remove & Check Handler functions

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
-function TodoItem({ itemData }) {
+function TodoItem({ itemData, todoRemoveHandler, todoCheckHandler }) {
   return (
     <>
       <p>{itemData.date}</p>
       <p>{itemData.content}</p>
+      <button onClick={() => todoCheckHandler(itemData.id)}>
+        {itemData.checked ? <p>미완료</p> : <p>완료</p>}
+      </button>
+      <button onClick={() => todoRemoveHandler(itemData.id)}>제거하기</button>
     </>
   )
 }
--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -8,11 +8,33 @@ function TodoList() {
     { id: 1, date: '2022-08-04', content: '공부하기', checked: false },
   ])
 
+  // todo 삭제 함수
+  const todoRemoveHandler = (id) => {
+    setTodoData(todoData.filter((itemData) => itemData.id !== id))
+  }
+
+  // todo check 여부
+  const todoCheckHandler = (id) => {
+    setTodoData(
+      todoData.map((itemData) =>
+        itemData.id === id
+          ? { ...itemData, checked: !itemData.checked }
+          : itemData,
+      ),
+    )
+  }
+
   return (
     <div>
       <TodoAdd todoId={todoId} todoData={todoData} setTodoData={setTodoData} />
       {todoData.map((itemData) => {
-        return <TodoItem itemData={itemData} />
+        return (
+          <TodoItem
+            itemData={itemData}
+            todoRemoveHandler={todoRemoveHandler}
+            todoCheckHandler={todoCheckHandler}
+          />
+        )
       })}
     </div>
   )
